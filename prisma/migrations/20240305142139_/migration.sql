dbfb64d33acfabb703f1b9bdcf1325e94016a7bf
6nsfour <diaryrakoto@gmail.com>
2024-03-05 15:24:18 +0100

    fix: delete useless migration

--- prisma/migrations/20240305142139_/migration.sql
+++ prisma/migrations/20240305142139_/migration.sql
@@ -2,10 +2,10 @@
   Warnings:
 
   - The primary key for the `Favorite` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `id` on the `Favorite` table. All the data in the column will be lost.
 
 */
 -- AlterTable
 ALTER TABLE "Favorite" DROP CONSTRAINT "Favorite_pkey",
-ALTER COLUMN "resource_id" DROP DEFAULT,
+DROP COLUMN "id",
 ADD CONSTRAINT "Favorite_pkey" PRIMARY KEY ("user_id", "resource_id");
-DROP SEQUENCE "Favorite_resource_id_seq";
